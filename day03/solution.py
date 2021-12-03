c4d0608189055c2069e040bbd0e54f6d21c8f3e1
codywp <codywp@users.noreply.github.com>
2021-12-03 14:02:16 -0600

    Update solution.py

--- day03/solution.py
+++ day03/solution.py
@@ -1,10 +1,10 @@
 import numpy as np
 from functools import reduce
 
-def solve1(input_array, num_inputs):
+def solve1(input_array):
     counts = np.sum(input_array, axis=0)
-    gamma = ''.join(['1' if bit >= num_inputs/2 else '0' for bit in counts])
-    epsilon = ''.join(['0' if bit >= num_inputs/2 else '1' for bit in counts])
+    gamma = ''.join(['1' if bit >= len(input_array)/2 else '0' for bit in counts])
+    epsilon = ''.join(['0' if bit >= len(input_array)/2 else '1' for bit in counts])
     print(int(gamma, 2) * int(epsilon, 2))
 
 def filter_list(array, col, trueVal, falseVal):
@@ -25,7 +25,6 @@ def solve2(input_array):
     )[0]
     print(int(''.join([str(i) for i in o2gen]), 2) * int(''.join([str(i) for i in co2scrub]), 2))
 
-input_list = [line.strip() for line in open('input.txt')]
-input_array = [[int(c) for c in line] for line in input_list]
-solve1(input_array, len(input_list))
+input_array = [[int(c) for c in line] for line in [line.strip() for line in open('input.txt')]]
+solve1(input_array)
 solve2(input_array)
